6835928c8aa3b8ad2dbcb912632fc4b7f2cfa0b4
khaled305 <khaled305.on@gmail.com>
2021-03-01 16:50:17 +0100

    first commit

---
+++ blog/tests.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Category, Post
+
+class Test_Create_Post(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        test_category = Category.objects.create(name='django')
+        testUser1 = User.objects.create_user(username='test_user', password='abcdefgh')
+        test_post = Post.objects.create(category_id=1, title='Post Title', excerpt='Post')
+        
+    def test_blog_content(self):
+        post = Post.postobjects.get(id=1)
+        cat = Category.objects.get(id=1)
+        author = f'{post.author}'
+        excerpt = f'{post.excerpt}'
+        title = f'{post.title}'
+        content = f'{post.content}'
+        status = f'{post.status}'
+        self.assertEqual(author, 'testUser1')
+        self.assertEqual(title, 'Post Title')
+        self.assertEqual(content, 'Post Content')
+        self.assertEqual(status, 'published')
+        self.assertEqual(str(post), 'Post Title')
+        self.assertEqual(str(cat), 'django')
